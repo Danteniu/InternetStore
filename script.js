c1d1a1a8104fb34acceff5c91947d9738093048a
Bellochka11 <elizavetabelyankina@mail.ru>
2024-08-04 18:01:09 +0300

    g

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const menuActive = document.querySelector('.menu'); // Находим меню
 const burger = document.querySelector('.head_buttons'); // Находим кнопку бургера
-const menuClose = document.querySelector('.menu-close'); // Находим кнопку крестика
 
 function toggleMenu() {
 menuActive.classList.toggle("hiddens"); //Ф-ция уделяет или устанавливает клесс с названием "hidden"
